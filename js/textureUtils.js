b5246765a6162d3e48323d4927e88cc63a6e2a8e
gameforming <96012344@student.theresialyceum.nl>
2026-03-08 21:13:20 +0100

    Update textureUtils.js

--- js/textureUtils.js
+++ js/textureUtils.js
@@ -1,34 +1,19 @@
 async function makeTransparent(img){
-
- let canvas=document.createElement("canvas")
- let ctx=canvas.getContext("2d")
-
- canvas.width=img.width
- canvas.height=img.height
-
- ctx.drawImage(img,0,0)
-
- let data=ctx.getImageData(0,0,canvas.width,canvas.height)
- let d=data.data
-
- for(let i=0;i<d.length;i+=4){
-
-  let r=d[i]
-  let g=d[i+1]
-  let b=d[i+2]
-
-  if(r>240 && g>240 && b>240){
-   d[i+3]=0
+  let c=document.createElement("canvas")
+  c.width=img.width
+  c.height=img.height
+  let ctx=c.getContext("2d")
+  ctx.drawImage(img,0,0)
+  let data=ctx.getImageData(0,0,c.width,c.height)
+  for(let i=0;i<data.data.length;i+=4){
+    if(data.data[i]===0 && data.data[i+1]===0 && data.data[i+2]===0){
+      data.data[i+3]=0
+    }
   }
-
- }
-
- ctx.putImageData(data,0,0)
-
- let newImg=new Image()
- newImg.src=canvas.toDataURL()
-
- await new Promise(res=>newImg.onload=res)
-
- return newImg
+  ctx.putImageData(data,0,0)
+  return new Promise(res=>{
+    let out=new Image()
+    out.src=c.toDataURL()
+    out.onload=()=>res(out)
+  })
 }
