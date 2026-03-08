a5bc2b6773426a059a2c668ebfa5b9f83eda4e23
gameforming <96012344@student.theresialyceum.nl>
2026-03-08 10:31:36 +0100

    Refactor game initialization and structure loading

--- js/main.js
+++ js/main.js
@@ -4,25 +4,40 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+
+
+// GAME DATA
 let blocks = {}
 let textures = {}
 
 let player
 let world
-let structures
 
+// structure manager global zodat world.js hem kan gebruiken
+window.structures = null
+
+
+
+// CAMERA
 let camera = {
 x:0,
 y:0
 }
 
+
+
+// LOAD BLOCKS
 async function loadBlocks(){
 
 let res = await fetch("data/blocks.json")
+
 blocks = await res.json()
 
 }
 
+
+
+// LOAD TEXTURES
 async function loadTextures(){
 
 for(let key in blocks){
@@ -33,9 +48,11 @@ if(textures[texName]) continue
 
 let img = new Image()
 
-img.src = "assets/"+texName
+img.src = "assets/" + texName
 
-await new Promise(r=>img.onload=r)
+await new Promise(resolve=>{
+img.onload = resolve
+})
 
 textures[texName] = img
 
@@ -43,48 +60,97 @@ textures[texName] = img
 
 }
 
+
+
+// INIT GAME
 async function init(){
 
 await loadBlocks()
+
 await loadTextures()
 
-structures = new StructureManager(blocks)
-await structures.loadStructure("tree")
 
+
+// load structures
+window.structures = new StructureManager(blocks)
+
+await window.structures.loadAll()
+
+
+
+// create player
 player = new Player()
 
+
+
+// create world
 world = new World(blocks,textures)
 
+
+
+// start game loop
 loop()
 
 }
 
+
+
+// UPDATE
 function update(){
 
 player.update()
 
+
+
+// camera follow
 camera.x = player.x - canvas.width/2
 camera.y = player.y - canvas.height/2
 
 }
 
+
+
+// DRAW
 function draw(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height)
 
+
+
+// world
 world.draw(ctx,camera)
 
+
+
+// player
 player.draw(ctx,camera)
 
 }
 
+
+
+// GAME LOOP
 function loop(){
 
 update()
+
 draw()
 
 requestAnimationFrame(loop)
 
 }
 
+
+
+// RESIZE SUPPORT
+window.addEventListener("resize",()=>{
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+})
+
+
+
+// START
 init()
