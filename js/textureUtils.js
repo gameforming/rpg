86bd309827745302452f1d15b4fb2e6bacf09e56
gameforming <96012344@student.theresialyceum.nl>
2026-03-09 16:30:24 +0100

    Update textureUtils.js

--- js/textureUtils.js
+++ js/textureUtils.js
@@ -1,19 +1,36 @@
-async function maketransparent(img){
-  let c=document.createElement("canvas")
-  c.width=img.width
-  c.height=img.height
-  let ctx=c.getContext("2d")
-  ctx.drawImage(img,0,0)
-  let data=ctx.getImageData(0,0,c.width,c.height)
-  for(let i=0;i<data.data.length;i+=4){
-    if(data.data[i]===0 && data.data[i+1]===0 && data.data[i+2]===0){
-      data.data[i+3]=0
+// textureUtils.js
+// Functie: maakt een afbeelding transparant waar pixels zwart zijn
+
+export async function makeTransparent(img){
+  // maak een tijdelijk canvas
+  const c = document.createElement("canvas")
+  c.width = img.width
+  c.height = img.height
+  const ctx = c.getContext("2d")
+
+  // teken afbeelding op canvas
+  ctx.drawImage(img, 0, 0)
+
+  // haal pixeldata op
+  const data = ctx.getImageData(0, 0, c.width, c.height)
+
+  // loop over alle pixels
+  for(let i = 0; i < data.data.length; i += 4){
+    const r = data.data[i]
+    const g = data.data[i+1]
+    const b = data.data[i+2]
+    // als pixel zwart, maak volledig transparant
+    if(r === 0 && g === 0 && b === 0){
+      data.data[i+3] = 0
     }
   }
-  ctx.putImageData(data,0,0)
-  return new Promise(res=>{
-    let out=new Image()
-    out.src=c.toDataURL()
-    out.onload=()=>res(out)
+
+  ctx.putImageData(data, 0, 0)
+
+  // maak een nieuwe Image en return als promise
+  return new Promise(res => {
+    const out = new Image()
+    out.src = c.toDataURL()
+    out.onload = () => res(out)
   })
 }
