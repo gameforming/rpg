93e3ba5204a99bd6d214c488fc3f4ef512abab8b
gameforming <96012344@student.theresialyceum.nl>
2026-03-09 12:10:47 +0100

    Update combat.js

--- js/combat.js
+++ js/combat.js
@@ -1,13 +1,17 @@
+// combat.js
+
 class Combat {
-  constructor(player){
-    this.player = player
-    this.level = 1
-    this.xp = 0
-    this.maxXp = 100 + 10*this.level
+  constructor(player, world){
+    this.player = player        // speler object
+    this.world = world          // world object, nodig voor tileSize
+    this.level = 1              // start level
+    this.xp = 0                 // start XP
+    this.maxXp = 100 + 10 * this.level
     this.lastAttack = 0
-    this.attackCooldown = 1000
+    this.attackCooldown = 1000  // 1 seconde cooldown
   }
 
+  // XP toevoegen en levels updaten
   gainXp(amount){
     this.xp += amount
     while(this.xp >= this.maxXp){
@@ -18,72 +22,82 @@ class Combat {
 
   levelUp(){
     this.level++
-    this.maxXp = Math.floor(this.maxXp*1.5)
+    this.maxXp = Math.floor(this.maxXp * 1.5)
   }
 
-  canAttack(time){ return time - this.lastAttack >= this.attackCooldown }
+  canAttack(time){
+    return time - this.lastAttack >= this.attackCooldown
+  }
 
+  // aanval functie
   attack(enemies, weapon, mouseX, mouseY, camera, time){
-    if(!this.canAttack(time)) return
+    if(!weapon || !this.canAttack(time)) return
     this.lastAttack = time
 
     let wx = this.player.x
     let wy = this.player.y
 
+    // gebruik weapon range of default
     let range = weapon.range || {width:3,length:3}
-    let attackWidth = range.width * window.world.tileSize
-    let attackLength = range.length * window.world.tileSize
+    let attackWidth = range.width * this.world.tileSize
+    let attackLength = range.length * this.world.tileSize
 
+    // richting van de muis
     let dx = mouseX + camera.x - wx
     let dy = mouseY + camera.y - wy
     let angle = Math.atan2(dy, dx)
-    let halfW = attackWidth / 2
 
+    // axis-aligned attack box in de richting van de muis
+    let halfW = attackWidth / 2
     let attackRect = {
-      x: wx + Math.cos(angle)*attackLength - halfW,
-      y: wy + Math.sin(angle)*attackLength - halfW,
+      x: wx + Math.cos(angle) * attackLength - halfW,
+      y: wy + Math.sin(angle) * attackLength - halfW,
       w: attackLength,
       h: attackWidth
     }
 
+    // damage toepassen op alle enemies in box
     for(let e of enemies.enemies){
       if(e.dead) continue
       if(e.x > attackRect.x && e.x < attackRect.x + attackRect.w &&
          e.y > attackRect.y && e.y < attackRect.y + attackRect.h){
         e.hp -= weapon.damage
         if(e.hp <= 0) e.dead = true
-        this.gainXp(10)
+        this.gainXp(10) // XP per kill/hit
       }
     }
   }
 
-  drawUI(ctx,canvas){
+  // UI tekenen: HP linksboven, Level+XP rechtsboven
+  drawUI(ctx, canvas){
     // HP linksboven
     let hpBarWidth = 200
-    let hp = this.player.hp
-    let maxHp = this.player.maxHp
-    ctx.fillStyle="black"
-    ctx.fillRect(20,20,hpBarWidth,20)
-    ctx.fillStyle="red"
-    ctx.fillRect(20,20,(hp/maxHp)*hpBarWidth,20)
-    ctx.strokeStyle="white"
-    ctx.strokeRect(20,20,hpBarWidth,20)
-    ctx.fillStyle="white"
-    ctx.font="16px Arial"
-    ctx.fillText(`HP: ${hp}/${maxHp}`,25,35)
+    let hp = this.player.hp || 1      // fallback als undefined
+    let maxHp = this.player.maxHp || 1
+    ctx.fillStyle = "black"
+    ctx.fillRect(20, 20, hpBarWidth, 20)
+    ctx.fillStyle = "red"
+    ctx.fillRect(20, 20, (hp/maxHp)*hpBarWidth, 20)
+    ctx.strokeStyle = "white"
+    ctx.strokeRect(20, 20, hpBarWidth, 20)
+    ctx.fillStyle = "white"
+    ctx.font = "16px Arial"
+    ctx.fillText(`HP: ${hp}/${maxHp}`, 25, 35)
 
-    // Level + XP rechtsboven
+    // Level + XP balk rechtsboven
     let xpBarWidth = 150
     let xPos = canvas.width - xpBarWidth - 20
-    ctx.fillStyle="black"
-    ctx.fillRect(xPos,20,xpBarWidth,20)
-    ctx.fillStyle="green"
-    ctx.fillRect(xPos,20,(this.xp/this.maxXp)*xpBarWidth,20)
-    ctx.strokeStyle="white"
-    ctx.strokeRect(xPos,20,xpBarWidth,20)
-    ctx.fillStyle="white"
-    ctx.fillText(`LV ${this.level}`, xPos,15)
+    ctx.fillStyle = "black"
+    ctx.fillRect(xPos, 20, xpBarWidth, 20)
+    ctx.fillStyle = "green"
+    ctx.fillRect(xPos, 20, (this.xp/this.maxXp)*xpBarWidth, 20)
+    ctx.strokeStyle = "white"
+    ctx.strokeRect(xPos, 20, xpBarWidth, 20)
+    ctx.fillStyle = "white"
+    ctx.font = "16px Arial"
+    ctx.fillText(`LV ${this.level}`, xPos, 15)
   }
 }
 
+// maak beschikbaar voor main.js
 window.combat = null
