bbd8a91a690459b1b18140a37b0a25f99f41f055
gameforming <96012344@student.theresialyceum.nl>
2026-03-09 14:48:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ canvas.width=window.innerWidth
 canvas.height=window.innerHeight
 import { Player } from "./player.js";
 import { makeTransparent } from "./textureUtils.js"
-import { canvas } from "./world.js"
+import { world } from "./world.js"
 
 
 
