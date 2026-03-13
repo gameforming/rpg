1d32c7f281f4355c8939fbfe622ca4be43fc80cb
gameforming <96012344@student.theresialyceum.nl>
2026-03-13 06:29:15 +0100

    Update textureUtils.js

--- js/textureUtils.js
+++ js/textureUtils.js
@@ -1,16 +1,9 @@
-export async function makeTransparent(src) {
-    const img = new Image();
-    img.src = src;
-
-    // Wacht tot de afbeelding volledig geladen is
-    await new Promise((resolve, reject) => {
-        img.onload = resolve;
-        img.onerror = reject;
-    });
+export async function makeTransparent(img) {
 
     const canvas = document.createElement("canvas");
     canvas.width = img.width;
     canvas.height = img.height;
+
     const ctx = canvas.getContext("2d");
 
     ctx.drawImage(img, 0, 0);
@@ -19,6 +12,7 @@ export async function makeTransparent(src) {
     const data = imageData.data;
 
     for (let i = 0; i < data.length; i += 4) {
+
         const r = data[i];
         const g = data[i + 1];
         const b = data[i + 2];
@@ -26,12 +20,14 @@ export async function makeTransparent(src) {
         if (r === 255 && g === 255 && b === 255) {
             data[i + 3] = 0;
         }
+
     }
 
     ctx.putImageData(imageData, 0, 0);
 
     const transparentImg = new Image();
     transparentImg.src = canvas.toDataURL();
+
     await new Promise(r => transparentImg.onload = r);
 
     return transparentImg;
